949483cf0f3465ad3c0032c2cb42620185faaf17
jeroenvanasten <jeroenva@magehost.pro>
2023-10-30 09:53:15 +0100

    removed OT

--- dist/render_post.js
+++ dist/render_post.js
@@ -44,7 +44,7 @@ export function renderPost(c) {
                     <div class="record"></div>
                 </div>
             </div>
-            <div class="post-row1">${c.finalTerm}</div>
+            <div class="post-row1">Ended</div>
         </div>
     </ha-card>
     `;    // Return the HTML template
